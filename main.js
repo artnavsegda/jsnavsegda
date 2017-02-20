86e9883e4bb6ac8c190121991f75c545c9ed3a69
Art Navsegda <artnavsegda@gmail.com>
2017-02-20 12:23:59 +0300

    more mayhem

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ console.log("hello javascript");
 
 function dropdata()
 {
-        xhttp.open("GET", "http://localhost:1100/getdata", false);
+        xhttp.open("GET", "http://192.168.1.111:1100/getdata", false);
         xhttp.send("hello");
         document.getElementById("demo").innerHTML = xhttp.responseText;
 }
