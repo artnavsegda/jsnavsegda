d92f1120b1ce10280bc55e4e0e1891f37174b949
Art Navsegda <artnavsegda@gmail.com>
2020-09-10 12:09:59 +0300

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -3,7 +3,10 @@ function main()
     console.log("Hello !");
     fetch("https://artnavsegda.herokuapp.com/q?ask=cat")
     .then(response => {
-        response.json());
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json());
     }
     .then(variants => {
         console.log(variants.toString());
