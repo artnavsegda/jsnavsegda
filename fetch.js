a4cd6b733550776405ee98a830d795913ae78eee
Art Navsegda <artnavsegda@gmail.com>
2020-09-10 11:36:51 +0300

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,4 +1,9 @@
 function main()
 {
     console.log("Hello !");
+    fetch("https://artnavsegda.herokuapp.com/q?ask=cat")
+    .then(response => response.json())
+    .then(variants => {
+        console.log(variants.toString());
+    })
 }
