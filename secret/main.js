e7aee74eda58d0b4ad85f3c54f36b05941e8f095
Art Navsegda <artnavsegda@gmail.com>
2024-06-08 00:13:10 +0300

    fg

--- secret/main.js
+++ secret/main.js
@@ -1,10 +1,4 @@
-var xhttp = new XMLHttpRequest();
-
 console.log("hello javascript");
 
-function dropdata()
-{
-        xhttp.open("GET", "http://192.168.1.111:1100/getdata", false);
-        xhttp.send("hello");
-        document.getElementById("demo").innerHTML = xhttp.responseText;
+function dropdata() {
 }
