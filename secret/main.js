4e55ea321ff00c9fab3df95627762c8cf71b81f7
Art Navsegda <artnavsegda@gmail.com>
2024-06-10 03:52:37 +0300

    ghf

--- secret/main.js
+++ secret/main.js
@@ -1,9 +1,28 @@
 function init() {
-    console.log("hello javascript");
+    printHash();
 }
 
-function printHash() {
-    console.log("print hash");
-    document.getElementById("clearResult").innerHTML = "clear";
-    document.getElementById("hashedResult").innerHTML = "hash";
+async function printHash() {
+    const clearValue = document.forms["data"]["secret"].value + ':'
+        + document.forms["data"]["guid"].value + ':'
+        + document.forms["data"]["version"].value + ':'
+        + document.forms["data"]["day"].value;
+
+    document.getElementById("clearResult").innerHTML = "Clear value: " + clearValue;
+
+    const buf = await crypto.subtle.digest('SHA-256', new TextEncoder("utf-8").encode(clearValue));
+
+    document.getElementById("hashedResult").innerHTML = "Hashed value: " + Array.prototype.map.call(new Uint8Array(buf), x => (('00' + x.toString(16)).slice(-2))).join('');
 }
+
+const text =
+    "An obscure body in the S-K System, your majesty. The inhabitants refer to it as the planet Earth.";
+
+async function digestMessage(message) {
+    const encoder = new TextEncoder();
+    const data = encoder.encode(message);
+    const hash = await window.crypto.subtle.digest("SHA-256", data);
+    return hash;
+}
+
+
