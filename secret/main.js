80edef23d0e292d4c4694949e5b9cc74f415a7c9
Art Navsegda <artnavsegda@gmail.com>
2024-06-10 03:22:17 +0300

    fds

--- secret/main.js
+++ secret/main.js
@@ -1,8 +1,9 @@
-console.log("hello javascript");
-
-function dropdata() {
+function init() {
+    console.log("hello javascript");
 }
 
 function printHash() {
     console.log("print hash");
+    document.getElementById("clearResult").innerHTML = "clear";
+    document.getElementById("hashedResult").innerHTML = "hash";
 }
